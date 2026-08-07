b5c5fb9aa597e63f5f01ce8d792d584cb7b37461
yusufnaser-app <abdulrahman250000@gmail.com>
2026-08-07 20:18:02 +0300

    Refactor build.gradle.kts for improved readability

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,35 +9,38 @@ plugins {
 val keystorePropertiesFile = rootProject.file("key.properties")
 val keystoreProperties = Properties()
 
-val hasKeystoreProperties = keystorePropertiesFile.exists()
-
-if (hasKeystoreProperties) {
+if (keystorePropertiesFile.exists()) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
+val hasKeystoreProperties = keystorePropertiesFile.exists()
+
 android {
+
     namespace = "com.yusufnaser.customsbroker"
 
-    // حل مشكلة file_picker و flutter_plugin_android_lifecycle
+    // مطلوب لـ file_picker و flutter_plugin_android_lifecycle
     compileSdk = 36
 
     ndkVersion = flutter.ndkVersion
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
     defaultConfig {
+
         applicationId = "com.yusufnaser.customsbroker"
 
         minSdk = flutter.minSdkVersion
 
-        // توافق Android الحديث
         targetSdk = 36
 
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+    }
+
+
+    compileOptions {
+
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
 
@@ -47,19 +50,14 @@ android {
 
             create("release") {
 
-                keyAlias =
-                    keystoreProperties["keyAlias"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
 
-                keyPassword =
-                    keystoreProperties["keyPassword"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+
+                storePassword = keystoreProperties["storePassword"] as String
 
                 storeFile =
-                    file(
-                        keystoreProperties["storeFile"] as String
-                    )
-
-                storePassword =
-                    keystoreProperties["storePassword"] as String
+                    file(keystoreProperties["storeFile"] as String)
             }
         }
     }
@@ -71,15 +69,14 @@ android {
 
             signingConfig =
                 if (hasKeystoreProperties) {
-
                     signingConfigs.getByName("release")
-
                 } else {
-
                     signingConfigs.getByName("debug")
                 }
 
+
             isMinifyEnabled = false
+
             isShrinkResources = false
         }
     }
